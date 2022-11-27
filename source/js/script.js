31fd2fb5f35268070930a7802d049e156f556f08
Антон Куклин <homyak91@inbox.ru>
2022-11-27 20:22:23 +0400

    add files

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  // Variables
+  let menuBtn = document.querySelector('.js-burger-btn');
+  let menu = document.querySelector('.js-menu');
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('open')
+  })
+
+
+  // Show menu
+
+
+})
